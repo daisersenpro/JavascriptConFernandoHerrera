a94614b2003e9d114dd718ed13a065c9eee7463d
Anyelo Eduardo Bustos Galdames <abustos@as-asoc.cl>
2026-05-20 14:28:35 -0400

    Basico de Javascript y consola

---
+++ assets/js/alerts.js
@@ -0,0 +1,10 @@
+//alert('Hola mundo!');
+
+//let nombre = prompt('¿Cómo te llamas?', 'Escribe tu nombre aquí');
+//console.log('*****' + nombre + '******');
+//console.log(nombre);
+
+//const seleccion = confirm('¿Estás seguro de eliminar este elemento?');
+//console.log(seleccion);
+
+console.log(global);
---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+alert('Hola Mundo'); // Muestra una ventana emergente con el mensaje "Hola Mundo" en el navegador.
+
+
+//console.log('Hola Mundo');
+
+let a = 10;
+let b = 20;
+let c = 'Hola';
+let d = 'Soy Spiderman';
+let e = 500;
+let f = 680;
+let x = a + c + d + e + b + f;
+
+const saludo = c + ' ' + d; // Concatenación de cadenas de texto para crear un saludo completo. Combina las variables 'c' y 'd' con un espacio entre ellas.
+
+//console.log('%cMis Variables', 'color: blue; font-size: 20px; font-weight: bold;'); //Estilo en console.log
+//console.log({a}); // {} Muestra el nombre de la variable y su valor, es una forma más clara de mostrar variables en la consola.
+//console.log({b});
+//console.log({c});
+//console.log({d});
+//console.log({e});
+//console.log({f});
+//console.log({x});
+
+//Imprime en modo Tabla console.table({a, b, c, d, e, f, x}); 
+//console.table({a, b, c, d, e, f, x}); // Muestra las variables en formato de tabla en la consola. Es una forma organizada de visualizar múltiples variables al mismo tiempo.    
+
+c = 'Hola de nuevo';
+
+var miNombre = 'Anyelo';
