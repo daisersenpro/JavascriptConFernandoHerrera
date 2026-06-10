2659574fddd6da0f977980065dee26e9bcd6c1fe
daisersenpro <sen.programador@gmail.com>
2026-06-10 17:56:12 -0400

    Todos los datos primitivos en javascript

---
+++ assets/js/primitivos.js
@@ -0,0 +1,35 @@
+let nombre = 'Abel';
+console.log(nombre);
+
+nombre = 'Abelardo';
+console.log(nombre);
+
+nombre = 'Abelardo Bustos';
+nombre = 'Abelardo Bustos Rodríguez';
+
+console.log(nombre);
+
+console.log(typeof nombre); //typeof es un operador que nos permite conocer el tipo de dato de una variable
+
+nombre = 123;
+console.log(nombre);
+console.log(typeof nombre);
+
+let esMayor = true;
+console.log(esMayor);
+console.log(typeof esMayor);
+
+let edad = 33;
+console.log(edad);
+console.log(typeof edad);
+
+let superPoderDeAbelardo;
+console.log(typeof superPoderDeAbelardo);
+
+let soyNull = null;
+console.log(soyNull);
+console.log(typeof soyNull); //typeof null es un error de JavaScript, debería ser 'null' pero devuelve 'object' por razones históricas
+
+let simbolo = Symbol('simbolo');
+console.log(simbolo);
+console.log(typeof simbolo);
