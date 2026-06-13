c6b0b5a1c863734f38843f6cf1c5b72b4b6f56f9
daisersenpro <sen.programador@gmail.com>
2026-06-12 22:54:01 -0400

    Palabras reservadas y poco de primitivos

---
+++ assets/js/palabras-reservadas.js
@@ -0,0 +1,15 @@
+let constante = 123;
+
+//const
+
+//if
+
+//try
+
+//while
+
+//Segmento de palabras que no pueden ser utilizadas como identificadores (nombres de variables, funciones, etc.) porque tienen un significado especial en el lenguaje JavaScript. Estas palabras reservadas son parte de la sintaxis del lenguaje y no pueden ser redefinidas o utilizadas para otros propósitos. Algunas de las palabras reservadas más comunes en JavaScript incluyen:
+
+//break, case, catch, class, const, continue, debugger, default, delete, do, else, export, extends, finally, for, function, if, import, in, instanceof, let, new, return, super, switch, this, throw, try, typeof, var, void, while, with, yield
+
+//nombres de variables no es bueno poner palabras con caracteres cmo ñ o acentos, aunque JavaScript lo permita, es mejor evitarlo para mantener la compatibilidad y evitar problemas de codificación en diferentes entornos. Es recomendable utilizar nombres de variables en inglés y sin caracteres especiales para asegurar la legibilidad y compatibilidad del código.
