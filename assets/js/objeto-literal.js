655792cf1180fd64aa254a5a9d93f83a2b69ed54
daisersenpro <sen.programador@gmail.com>
2026-08-11 12:34:50 -0400

    Clase Objeto Literal mas detalles

--- assets/js/objeto-literal.js
+++ assets/js/objeto-literal.js
@@ -30,3 +30,20 @@ const x = 'vivo';
 console.log('Vivo:', personaje[x]); // Muestra si el personaje esta vivo o no
 
 console.log('Ultima pelicula:', personaje.ultimaPelicula); // Muestra la ultima pelicula del personaje
+
+//Mas detalles del objeto
+
+delete personaje.edad; // Elimina la propiedad edad del objeto personaje
+console.log(personaje); // Muestra el objeto sin la propiedad edad
+
+personaje.casado = true; // Agrega la propiedad casado al objeto personaje
+
+const entriesPares = Object.entries(personaje); // Convierte el objeto en un array de pares [key, value]
+console.log(entriesPares); // Muestra el array de pares [key, value]
+
+Object.freeze(personaje); // Congela el objeto, no se pueden agregar, eliminar o modificar propiedades
+personaje.dinero = 10000000000000;
+
+const propiedades = Object.getOwnPropertyNames(personaje); // Obtiene un array con los nombres de las propiedades del objeto
+const valores = Object.values(personaje); // Obtiene un array con los valores de las propiedades del objeto     
+console.log({propiedades, valores}); // Muestra el array con los nombres de las propiedades del objeto
