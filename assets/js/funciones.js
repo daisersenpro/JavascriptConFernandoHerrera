4576e2161c31c7f25515dc2574f487a3f6aab992
daisersenpro <sen.programador@gmail.com>
2026-08-13 21:07:56 -0400

    Funciones y funciones de Flecha part 2

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,5 +1,8 @@
 function saludar(){
     console.log("Hola mundo");// funcion declarada
+    return [1,2,3,4,5];
+
+    console.log("Soy un codigo que esta ");// funcion declarada
 }
 
 
@@ -9,6 +12,8 @@ const saludar2 = function(){
 
 function saludar3(nombre){
     console.log('Hola '+ nombre); // funcion con parametros
+
+    
 }
 
 const saludar4 = function(nombre){
@@ -24,9 +29,33 @@ const saludarFlecha2 = (nombre) => {
     console.log('Hola ' + nombre); // funcion flecha con parametros
 }
 
-saludar();//imprime funcion declarada
+const retornoSaludar = saludar();//retorno de funcion declarada
+console.log(retornoSaludar);//imprime retorno de funcion declarada
+
+/* saludar();//imprime funcion declarada
 saludar2();//imprime funcion anonima
 saludar3('Fernando Herrera');//imprime funcion con parametros
 saludar4('Fernando Herrera 4', 40, true, 'Paris');//imprime funcion con parametros y constante
 saludarFlecha();//imprime funcion flecha
-saludarFlecha2('Hola desde Flecha 2');//imprime funcion flecha con parametros
+saludarFlecha2('Hola desde Flecha 2');//imprime funcion flecha con parametros */
+
+function sumar(a,b){ //funcion declarada con parametros y retorno
+    return a + b;
+}
+
+const sumar2 = (a,b) => a + b; //funcion flecha con parametros y retorno
+
+function getAleatorio(){
+    return Math.random();
+}
+
+const getAleatorio2 = () => Math.random(); //funcion flecha con retorno alternativo sin llaves
+
+//imprime funcion sumar2 con parametros y retorno
+console.log(sumar2(10,20));//imprime funcion sumar2 con parametros y retorno
+
+console.log(sumar(10,20));//imprime funcion sumar con parametros y retorno
+
+console.log(getAleatorio());//imprime funcion getAleatorio con retorno
+
+console.log(getAleatorio2());//imprime funcion getAleatorio2 con retorno
